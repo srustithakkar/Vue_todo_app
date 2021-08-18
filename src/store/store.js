0f1fc712ee01053355c231a2e0e4dd98a32b9c35
Srusti Thakkar <srusti.thakkar@cygnet.com>
2021-08-18 15:42:15 +0530

    added vuex-persistedstate for storing todos in local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
@@ -8,6 +9,7 @@ export default new Vuex.Store({
         todos: [],
         newTodo: ''
     },
+    plugins: [createPersistedState()],
     mutations: {
         GET_TODO(state, todo){
             state.newTodo =  todo
